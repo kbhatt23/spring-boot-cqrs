3e9cafe0a1e2957cb4c29ae9a651a8150c4aa481
kbhatt23 <kanishklikesprogramming@gmail.com>
2020-12-25 22:54:57 +0530

    User Service base project using AXON with Domain driven design.

---
+++ codebase/user-command-service/src/main/java/com/learning/cqrs/user_service/UserCommandServiceApplication.java
@@ -0,0 +1,13 @@
+package com.learning.cqrs.user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCommandServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCommandServiceApplication.class, args);
+	}
+
+}
---
+++ codebase/user-query-service/src/main/java/com/learning/cqrs/user_service/UserQueryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.learning.cqrs.user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserQueryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserQueryServiceApplication.class, args);
+	}
+
+}
